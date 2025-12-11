60d6f66f3fb3c290d781922e88bf5f0ee0185590
akatakam <aditya.katakam@gmail.com>
2025-12-11 20:11:05 +0530

    learning terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   count = 9
   ami = var.ami
   instance_type = var.instance_type
-  vpc_security_group_ids = [var.vpc_security_group_ids]
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     name = var.components[count.index]
@@ -12,8 +12,8 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "a-records" {
   count = 9
   zone_id = var.zone_id
-  name    = "${var.component[count.index]}-dev"
-  type    = A
+  name    = "${var.components[count.index]}-dev"
+  type    = "A"
   ttl     = 30
   records = [aws_instance.instances[count.index].private_ip]
 }
